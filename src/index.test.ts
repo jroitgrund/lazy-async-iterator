d4196c7704952af93d509b7ed2602d952a0eef3f
Jonathan Roitgrund <jroitgrund@palantir.com>
2017-10-09 15:13:45 +0100

    Use async generators

--- src/index.test.ts
+++ src/index.test.ts
@@ -21,10 +21,12 @@ const postsAsyncFetcher = {
 
 describe("getInfiniteStream", () => {
     it("returns an infinite stream", async () => {
-        const iterator = getInfiniteStream(postsAsyncFetcher)[Symbol.iterator]();
         const results: number[] = [];
-        for (let i = 0; i < 10; i++) {
-            results.push((await iterator.next()).value);
+        for await (const result of getInfiniteStream(postsAsyncFetcher)) {
+            results.push(result);
+            if (results.length === 10) {
+                break;
+            }
         }
         expect(results).toEqual([0, 0, 10, 11, 20, 22, 30, 33, 40, 44]);
     });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-export interface IAsyncIterable<T> {
-    [Symbol.iterator]: () => IAsyncIterator<T>;
-}
-
-export interface IAsyncIterator<T> {
-    next: () => Promise<{ done: boolean; value: T }>;
-}
+(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
 
 export interface IAsyncFetcher<T, R, P> {
     get: (after?: T) => Promise<P>;
@@ -12,33 +6,11 @@ export interface IAsyncFetcher<T, R, P> {
     getResults: (page: P) => R[];
 }
 
-export function getInfiniteStream<T, R, P>(
-    asyncFetcher: IAsyncFetcher<T, R, P>
-): IAsyncIterable<R> {
-    let currentResultsIterator: Iterator<R> = [][Symbol.iterator]();
-    let nextToken: T;
-
-    return {
-        [Symbol.iterator]: () => ({
-            next(): Promise<{ done: boolean; value: R }> {
-                const { done, value } = currentResultsIterator.next();
-                if (done) {
-                    return asyncFetcher.get(nextToken).then(page => {
-                        nextToken = asyncFetcher.getToken(page);
-                        const results = asyncFetcher.getResults(page);
-                        currentResultsIterator = results[Symbol.iterator]();
-                        return {
-                            done: false,
-                            value: currentResultsIterator.next().value,
-                        };
-                    });
-                } else {
-                    return Promise.resolve({
-                        done: false,
-                        value,
-                    });
-                }
-            },
-        }),
-    };
+export async function* getInfiniteStream<T, R, P>(asyncFetcher: IAsyncFetcher<T, R, P>) {
+    let token: T | undefined;
+    while (true) {
+        const page = await asyncFetcher.get(token as T);
+        token = asyncFetcher.getToken(page);
+        yield* asyncFetcher.getResults(page);
+    }
 }
